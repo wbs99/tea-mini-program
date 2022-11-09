dfe0df198aec120f8c145c77aa56c7bae55fbabd
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-09 15:03:30 +0800

    项目初始化

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,44 @@
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName')
+  },
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs',
+    })
+  },
+  onLoad() {
+    // @ts-ignore
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+  },
+  getUserProfile() {
+    wx.getUserProfile({
+      desc: '展示用户信息',
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e: any) {
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
