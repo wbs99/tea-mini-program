ca12f60b9a0300744454f4b12fa80db5acfbfe61
dingtalk_xsgrpl <1134954328@qq.com>
2022-11-22 10:15:02 +0800

    完成我的页面样式

--- miniprogram/pages/me/index.ts
+++ miniprogram/pages/me/index.ts
@@ -1,11 +1,32 @@
 Page({
 
   data: {
-
+    paddingTop: 0,
+    menuList: [
+      {
+        title: '兑换码',
+        icon: 'qr'
+      },
+      {
+        title: '隐私协议',
+        icon: 'shield-o'
+      }, {
+        title: '用户服务协议',
+        icon: 'records'
+      },
+      {
+        title: '经营信息公示',
+        icon: 'notes-o'
+      }
+    ]
   },
 
   onLoad() {
-
+    // 设置页面顶部的安全距离
+    const { bottom } = wx.getMenuButtonBoundingClientRect()
+    this.setData({
+      paddingTop: bottom
+    })
   },
   onShow() {
     this.getTabBar().init()
