96f8c658ffed0c6c8cd6c73908b846900e53f586
dingtalk_xsgrpl <1134954328@qq.com>
2022-12-03 17:20:07 +0800

    根据起点终点经纬度计算两点距离

--- miniprogram/pages/store/index.ts
+++ miniprogram/pages/store/index.ts
@@ -3,10 +3,12 @@ import { StoreStatus } from '../../enums/StoreStatus';
 import { fetchStoreListApi } from './../../apis/store';
 import { MapMarker } from './types';
 const computedBehavior = require('miniprogram-computed').behavior
+import QQMapWX from "@jonny1994/qqmap-wx-jssdk";
 
 
 Page({
   behaviors: [computedBehavior],
+  mapSdk: <QQMapWX | null>null,
   data: {
     paging: <Paging>{
       page: 1,
@@ -15,8 +17,12 @@ Page({
     },
     storeList: <Store[]>[],
     storeStatusMap: StoreStatus,
-    currentLocation: <Location | null>null,
-    markers: <MapMarker[]>[]
+    currentLocation: <Location>{
+      latitude: 0,
+      longitude: 0
+    },
+    markers: <MapMarker[]>[],
+    distance: 0
   },
   computed: {
     markers(data: { storeList: Store[] }): MapMarker[] {
@@ -33,27 +39,47 @@ Page({
       })
     }
   },
-
+  initMapSdk() {
+    this.mapSdk = new QQMapWX({
+      key: "TKGBZ-FEQL4-3PKUL-XER3K-774R2-3FBZ6",
+    });
+  },
+  // 根据经纬度计算起点终点距离
+  calculateDistanceAndSetStoreList(storeList: Store[]) {
+    const locationList: Location[] = storeList.map(item => item.location)
+    this.mapSdk?.calculateDistance({
+      from: this.data.currentLocation,
+      to: locationList,
+      success: (res) => {
+        storeList.forEach((item, index) => {
+          // @ts-ignore
+          storeList[index]['distance'] = (res.result.elements[index].distance / 1000).toFixed(2)
+        })
+        this.setData({
+          storeList
+        })
+      }
+    })
+  },
   async fetchStoreList() {
     const { paging, data } = await fetchStoreListApi()
+    data.length && this.calculateDistanceAndSetStoreList(data)
     this.setData({
       paging,
       storeList: data
     })
   },
-
-  onLoad() {
-    wx.getLocation({
-      type: 'wgs84',
-      success: (res) => {
-        const { latitude, longitude } = res
-        this.setData({
-          currentLocation: {
-            latitude, longitude
-          }
-        })
+  async fetchCurrentLocation() {
+    const { latitude, longitude } = await wx.getLocation({ type: 'wgs84' })
+    this.setData({
+      currentLocation: {
+        latitude, longitude
       }
     })
+  },
+  async onLoad() {
+    this.initMapSdk()
+    await this.fetchCurrentLocation()
     this.fetchStoreList()
   },
   onShow() {
